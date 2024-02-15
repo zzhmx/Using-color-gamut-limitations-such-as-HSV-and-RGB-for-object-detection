2ae3386719ef71add76b93b69549817ea62dbb0b
zzhmx <58509426+zzhmx@users.noreply.github.com>
2024-02-15 20:01:00 +0800

    Add files via upload

---
+++ opencv_huakuai.py
@@ -0,0 +1,61 @@
+#不可用于商用#
+
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+def Calibrate_size(lower,upper):
+    if lower[0]<=upper[0] and lower[1]<=upper[1] and lower[2]<=upper[2] :
+        return 1
+    else :
+        return 0
+
+cap = cv2.VideoCapture(0)
+frame = cv2.imread("F://3151.jpg")
+
+while 1:
+    # ret为是否找到图像， frame_cap是帧本身
+    ret, frame_cap = cap.read()
+    if ret:
+        cv2.namedWindow("cap", 0);
+        cv2.resizeWindow("cap", 1080, 720);
+        cv2.imshow('cap', frame_cap)
+        if cv2.waitKey(1) == ord(' '): #按下空格截下当前帧用于色域筛选
+            frame=frame_cap
+            cv2.destroyAllWindows()
+            cap.release()
+            break
+cv2.namedWindow('img',cv2.WINDOW_NORMAL)
+cv2.resizeWindow("img", 400, 200);
+cv2.createTrackbar('H_max','img',179,179,nothing)
+cv2.createTrackbar('H_min','img',0,179,nothing)
+cv2.createTrackbar('S_max','img',255,255,nothing)
+cv2.createTrackbar('S_min','img',0,255,nothing)
+cv2.createTrackbar('V_max','img',255,255,nothing)
+cv2.createTrackbar('V_min','img',0,255,nothing)
+# 将图像转换到 HSV 空间
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+while (1):
+    h_max = cv2.getTrackbarPos('H_max', 'img')
+    h_min = cv2.getTrackbarPos('H_min', 'img')
+    s_max = cv2.getTrackbarPos('S_max', 'img')
+    s_min = cv2.getTrackbarPos('S_min', 'img')
+    v_max = cv2.getTrackbarPos('V_max', 'img')
+    v_min = cv2.getTrackbarPos('V_min', 'img')
+    lower_hsv=np.array([h_min,s_min,v_min])
+    upper_hsv=np.array([h_max,s_max,v_max])
+    if Calibrate_size(lower_hsv, upper_hsv) == 1:
+        # 设置阈值图像中只出现hsv范围的物件
+        mask = cv2.inRange(hsv, lower_hsv, upper_hsv)#二值
+        img_useful = cv2.bitwise_and(frame, frame, mask=mask)  # 与操作
+        cv2.namedWindow("capture", cv2.WINDOW_NORMAL);
+        cv2.resizeWindow("capture", 1080, 720);
+        cv2.imshow('capture', img_useful)
+    else :
+        print('三个通道最大值需要大于最小值/The maximum value of the three channels needs to be greater than the minimum value')
+    if cv2.waitKey(1) == 27 or cv2.waitKey(1) == ord('q'):
+        cv2.destroyAllWindows()
+        break
+
---
+++ readhsv.py
@@ -0,0 +1,56 @@
+#不可用于商用#
+
+import cv2
+import numpy as np
+def getROI(event, x, y, flags, param):
+    global ix, iy, drawing, img, hsv_img, hsv_roi, box ,img2
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            img2=img.copy()
+            cv2.rectangle(img2, (ix, iy), (x, y), (0, 255, 0), 2)
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        box = (min(ix,x), min(iy,y), abs(ix-x), abs(iy-y))
+        roi = hsv_img[box[1]:box[1]+box[3], box[0]:box[0]+box[2], :]
+        h_min, s_min, v_min = np.min(roi, axis=(0,1))
+        h_max, s_max, v_max = np.max(roi, axis=(0,1))
+        print("Hue: ", h_min, h_max)
+        print("Saturation: ", s_min, s_max)
+        print("Value: ", v_min, v_max)
+        #cv2.imshow("ROI", roi)
+        hsv_roi[:] = (h_min, s_min, v_min), (h_max, s_max, v_max)
+        img2=img.copy()
+# 读取图像
+img = cv2.imread("F://3151.jpg")
+cap = cv2.VideoCapture(0)
+while 1:
+    # ret为是否找到图像， frame_cap是帧本身
+    ret, frame_cap = cap.read()
+    if ret:
+        cv2.namedWindow("cap", 0);
+        cv2.resizeWindow("cap", 1080, 720);
+        cv2.imshow('cap', frame_cap)
+        if cv2.waitKey(1) == ord(' '): #按下空格截下当前帧用于读取色域范围
+            img=frame_cap
+            cv2.destroyAllWindows()
+            cap.release()
+            break
+img2=img.copy()
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hsv_roi = np.zeros((1, 2, 3))
+# 定义窗口并绑定事件
+cv2.namedWindow("image",0)
+cv2.resizeWindow("image", 640, 480)
+cv2.setMouseCallback("image", getROI)
+drawing = False
+# 循环显示图像
+while True:
+    cv2.imshow("image", img2)
+    k = cv2.waitKey(1) & 0xFF
+    if cv2.waitKey(1) == 27:  # esc退出
+        break
+# 释放资源
+cv2.destroyAllWindows()
